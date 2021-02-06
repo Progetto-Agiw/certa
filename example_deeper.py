19627b503df75f2220ea4de8bb1ac698d5a683ff
Antonio Turco <truock@email.com>
2021-02-06 15:06:07 +0100

    :recycle: loads a model if already saved in the filesystem

--- example_deeper.py
+++ example_deeper.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import gensim.downloader as api
 import models.DeepER as dp
+from keras.models import load_model
 from certa.local_explain import find_similarities
 from certa.local_explain import dataset_local
 from certa.triangles_method import explainSamples
@@ -10,7 +11,8 @@ from certa.eval import expl_eval
 
 
 def merge_sources(table, left_prefix, right_prefix, left_source, right_source, copy_from_table, ignore_from_table):
-    dataset = pd.DataFrame(columns={col: table[col].dtype for col in copy_from_table})
+    dataset = pd.DataFrame(
+        columns={col: table[col].dtype for col in copy_from_table})
     ignore_column = copy_from_table + ignore_from_table
 
     for _, row in table.iterrows():
@@ -40,14 +42,16 @@ def to_deeper_data(df: pd.DataFrame):
         rpd = row.filter(regex='^rtable_')
         if 'label' in row:
             label = row['label']
-            res.append((lpd.values.astype('str'), rpd.values.astype('str'), label))
+            res.append((lpd.values.astype('str'),
+                        rpd.values.astype('str'), label))
         else:
             res.append((lpd.values.astype('str'), rpd.values.astype('str')))
     return res
 
 
 def predict_fn(x, m, ignore_columns=['ltable_id', 'rtable_id', 'label']):
-    data = to_deeper_data(x.drop([c for c in ignore_columns if c in x.columns], axis=1))
+    data = to_deeper_data(
+        x.drop([c for c in ignore_columns if c in x.columns], axis=1))
     out = dp.predict(data, model, embeddings_model, tokenizer)
     out_df = pd.DataFrame(out, columns=['nomatch_score', 'match_score'])
     out_df.index = x.index
@@ -62,7 +66,8 @@ def get_original_prediction(r1, r2):
     r1_df.columns = list(map(lambda col: lprefix + col, r1_df.columns))
     r2_df.columns = list(map(lambda col: rprefix + col, r2_df.columns))
     r1r2 = pd.concat([r1_df, r2_df], axis=1)
-    r1r2['id'] = "0@" + str(r1r2[lprefix + 'id'].values[0]) + "#" + "1@" + str(r1r2[rprefix + 'id'].values[0])
+    r1r2['id'] = "0@" + str(r1r2[lprefix + 'id'].values[0]) + \
+        "#" + "1@" + str(r1r2[rprefix + 'id'].values[0])
     r1r2 = r1r2.drop([lprefix + 'id', rprefix + 'id'], axis=1)
     return predict_fn(r1r2, model)[['nomatch_score', 'match_score']].values[0]
 
@@ -74,9 +79,12 @@ gt = pd.read_csv(datadir + 'train.csv')
 valid = pd.read_csv(datadir + 'valid.csv')
 test = pd.read_csv(datadir + 'test.csv')
 
-train_df = merge_sources(gt, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
-valid_df = merge_sources(valid, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
-test_df = merge_sources(test, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
+train_df = merge_sources(gt, 'ltable_', 'rtable_',
+                         lsource, rsource, ['label'], ['id'])
+valid_df = merge_sources(valid, 'ltable_', 'rtable_',
+                         lsource, rsource, ['label'], ['id'])
+test_df = merge_sources(test, 'ltable_', 'rtable_',
+                        lsource, rsource, ['label'], ['id'])
 
 if not os.path.exists('models/glove.6B.50d.txt'):
     word_vectors = api.load("glove-wiki-gigaword-50")
@@ -85,9 +93,15 @@ if not os.path.exists('models/glove.6B.50d.txt'):
 embeddings_index = dp.init_embeddings_index('models/glove.6B.50d.txt')
 emb_dim = len(embeddings_index['cat'])
 embeddings_model, tokenizer = dp.init_embeddings_model(embeddings_index)
-model = dp.init_DeepER_model(emb_dim)
 
-model = dp.train_model_ER(to_deeper_data(train_df), model, embeddings_model, tokenizer)
+
+if os.path.exists("DeepER_best_model.h5"):
+    model = load_model("DeepER_best_model.h5")
+else:
+    model = dp.init_DeepER_model(emb_dim)
+    model = dp.train_model_ER(to_deeper_data(
+        train_df), model, embeddings_model, tokenizer)
+
 
 theta_min, theta_max = find_similarities(test_df, False)
 
